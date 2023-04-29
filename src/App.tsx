28ff5493b1269e90061f954eaece99340f8a7b88
TheFatPanda97 <shawnhu97@gmail.com>
2023-04-28 22:34:13 -0400

    working on things

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Webcam from 'react-webcam';
+import * as tf from '@tensorflow/tfjs';
+import { nextFrame } from '@tensorflow/tfjs';
+
+const App = () => {
+  // document.body.requestFullscreen();
+
+  const videoConstraints = {
+    width: 1920,
+    height: 720,
+    // facingMode: { exact: 'user' },
+    facingMode: 'user',
+  };
+
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef(null);
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network
+    // e.g. const net = await cocossd.load();
+    // https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json
+    const net = await tf.loadGraphModel(
+      'https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json',
+    );
+
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 16.7);
+  };
+
+  const detect = async (net: tf.GraphModel) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // // Set canvas height and width
+      // canvasRef.current.width = videoWidth;
+      // canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [640, 480]);
+      const casted = resized.cast('int32');
+      const expanded = casted.expandDims(0);
+      const obj = await net.executeAsync(expanded);
+      console.log(obj);
+
+      // const boxes = await obj[1].array();
+      // const classes = await obj[2].array();
+      // const scores = await obj[4].array();
+
+      // Draw mesh
+      // const ctx = canvasRef.current.getContext('2d');
+
+      // 5. TODO - Update drawing utility
+      // drawSomething(obj, ctx)
+      // requestAnimationFrame(() => {
+      //   drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx);
+      // });
+
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
+    }
+  };
+
+  useEffect(() => {
+    // runCoco();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Webcam
+        ref={webcamRef}
+        muted
+        videoConstraints={videoConstraints}
+        width={100}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      />
+      <button onClick={() => document.body.requestFullscreen()}>click me</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+    }),
+  ],
+});
