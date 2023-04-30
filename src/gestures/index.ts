328bc90b76f6617bf918ebd57770b38fc15e8e5a
TheFatPanda97 <shawnhu97@gmail.com>
2023-04-29 20:46:31 -0400

    some more tings
    Co-authored-by: SleepyJesse <Jesse_Xu@live.com>
    Co-authored-by: William Chau <williamchau123@users.noreply.github.com>

--- src/gestures/index.ts
+++ src/gestures/index.ts
@@ -3,6 +3,6 @@ import loveGesture from './ILoveYou';
 import noGesture from './No';
 import yesGesture from './Yes';
 import whyGesture from './Why';
-import eSpell from './e';
+// import eSpell from './e';
 
-export default [whyGesture, helloGesture, loveGesture, noGesture, yesGesture, eSpell];
+export default [whyGesture, helloGesture, loveGesture, noGesture, yesGesture];
